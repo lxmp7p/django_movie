cc36b27dfdb6f7180fec170abdda0ef708551089
Zaur <lxmp7p@yandex.ru>
2021-08-30 17:15:24 +0300

    added author pages

--- movies/views.py
+++ movies/views.py
@@ -2,22 +2,30 @@ from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
 
-from .models import Movie, Category
+from .models import Movie, Category, Actor, Genre
 from .forms import ReviewForm
 
 
+class GenreYear:
+    """Жанры и года выхода фильмов"""
+    def get_genres(self):
+        return Genre.objects.all()
 
-class MoviesView(ListView):
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values("year")
+
+
+class MoviesView(GenreYear, ListView):
     """Список фильмов"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     """Полное описание фильма"""
     model = Movie
     slug_field = 'url'
 
-class AddReview(View):
+class AddReview(GenreYear, View):
     """Отзывы"""
     def post(self, request, pk):
         form = ReviewForm(request.POST)
@@ -29,3 +37,9 @@ class AddReview(View):
             form.movie = movie
             form.save()
         return redirect(movie.get_absolute_url())
+
+class ActorView(GenreYear, DetailView):
+    """Вывод информации о актере"""
+    model = Actor
+    template_name = "movies/actor.html"
+    slug_field = "name"
